1f889fe60a4f7c6a798bc6e67b68e8191a4d1c14
github username <github email address>
2018-10-19 14:35:33 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,77 @@
 require_relative "../config/environment.rb"
 
 class Student
-
+attr_accessor :name, :grade
+attr_reader :id
+def initialize(id=nil, name, grade)
+  @name = name
+  @grade = grade
+  @id = id
+end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    );
+    SQL
+    DB[:conn].execute(sql)
+  end
 
-end
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  # This instance method inserts a new row into the database using the attributes of the given object. This method also assigns the id attribute of the object once the row has been inserted into the database.
+  #use update method inside save method
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES (?, ?);
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+# However, at the end of your #save method, you do need to grab the ID of the last inserted row, i.e. the row you just inserted into the database, and assign it to the be the value of the @id attribute of the given instance
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students;")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    new_student = self.new(row[0], row[1], row[2])
+    new_student
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+end #end of student class
